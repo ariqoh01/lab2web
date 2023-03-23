cfc76e4f4be6974379e0a7830ca6fc304d583a62
ariqoh01 <92715909+ariqoh01@users.noreply.github.com>
2023-03-23 11:25:09 +0700

    Add files via upload

---
+++ php_dasar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php
+echo "Hello World";
+?>
+<h2>menggunakan variable</h2>
+<?php 
+$nim = "312110138";
+$nama = 'Ariqoh Zulaika Zuhrah';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+
+echo 'Selamat Datang ' . $_GET['nama'];
+?>
+
+</body>
+</html>
